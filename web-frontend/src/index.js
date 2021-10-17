e65b721f7767e30446ce3d6fa8ba33d776aa4267
sush104 <2642782s@student.gla.ac.uk>
2021-10-17 11:26:06 +0100

    Dashboard, Reapir screen are working(still need few changes)

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -2,9 +2,9 @@ import ReactDOM from "react-dom";
 //import { createBrowserHistory } from "history";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Signup from "./components/Signup";
-import Signin from "./components/Signin";
-
+import Signup from "./components/Signup/Signup";
+import Signin from "./components/Signin/Signin";
+import Admin from "layouts/Admin.js";
 //const hist = createBrowserHistory();
 
 ReactDOM.render(
@@ -12,9 +12,9 @@ ReactDOM.render(
       <Router>
         {/* <Navbar /> */}
         <Switch>  
-        <Route path='/' component={Signin}  exact/>
+        <Route path='/' component={Signin}  exact={true}/>
         <Route  path='/signup' component={Signup} />
-      
+        <Route path="/admin" component={Admin} />
         </Switch>
       </Router>
   </Router>,
